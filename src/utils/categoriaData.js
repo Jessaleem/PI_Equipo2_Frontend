d4305dbee7f9541b2770ba1d691e7f12ce4ee5a4
xxzarkx <rodrigofranco.1301@gmail.com>
2024-10-29 20:21:16 -0500

    subiendo cambio en ruta de imagenes

--- src/utils/categoriaData.js
+++ src/utils/categoriaData.js
@@ -1,22 +1,27 @@
+import categoriaGastronomia from "@assets/categoriaGastronomia.png";
+import categoriaCultura from "@assets/categoriaCultura.png";
+import categoriaArte from "@assets/categoriaArte.png";
+import categoriaNaturaleza from "@assets/categoriaNaturaleza.png";
+
 export let categoriaData = [
   {
     id: 1,
     categoria: "gastronomia",
-    imagen: "src/assets/categoriaGastronomia.png",
+    imagen: categoriaGastronomia,
   },
   {
     id: 2,
     categoria: "cultura",
-    imagen: "src/assets/categoriaCultura.png",
+    imagen: categoriaCultura,
   },
   {
     id: 3,
     categoria: "arte",
-    imagen: "src/assets/categoriaArte.png",
+    imagen: categoriaArte,
   },
   {
     id: 4,
     categoria: "naturaleza",
-    imagen: "src/assets/categoriaNaturaleza.png",
+    imagen: categoriaNaturaleza,
   },
 ];
