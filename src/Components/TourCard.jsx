d8e3b52df2dcc77fcc062106a88a536b9948072f
Quecky <dalisbarranco@hotmail.com>
2024-10-30 19:53:33 -0600

    fix-productos2

--- src/Components/TourCard.jsx
+++ src/Components/TourCard.jsx
@@ -4,17 +4,18 @@ import { Link } from 'react-router-dom';
 const TourCard = ({ imagen, nombre, id }) => {
   return (
     <div
-      className='position-relative rounded-2 overflow-hidden max-width-250px'
-      style={{ maxWidth: '250px', minHeight: '334px' }}
+      className='tour-card position-relative rounded-2 overflow-hidden'
+   
     >
       <img
         src={imagen}
         className='h-100 w-100'
+        alt={`Imagen de ${nombre}`}
       />
       <div className='position-absolute z-1 bottom-0 w-100 py-3 d-flex flex-column align-items-center gap-2 footer-tourCard-colors'>
-        <h3 className='m-0'>{nombre}</h3>
+        <h3 className='m-0 text-center'>{nombre}</h3>
         <Link to={`/tour/${id}`}>
-          <button className='w-50 btn py-2 fs-5 fw-bold ver-tourCard-btn-colors'>
+          <button className='w-75 btn py-2 fs-6 fw-bold ver-tourCard-btn-colors'>
             Ver
           </button>
         </Link>
