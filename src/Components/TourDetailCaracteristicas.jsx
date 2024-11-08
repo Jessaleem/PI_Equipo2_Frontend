b42b1e27af349a0e456e418f723f00d4ac630506
xxzarkx <rodrigofranco.1301@gmail.com>
2024-11-08 18:11:14 -0500

    Visualizar caracteristicas del producto

---
+++ src/Components/TourDetailCaracteristicas.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TourDetailCaracteristicas = ({ caracteristicas }) => {
+  return (
+    <div className="text-start px-3">
+      <div className="w-max-1280">
+        <h2 className="text-center my-5">Qué incluye:</h2>
+        <section className="d-flex flex-wrap justify-content-center justify-content-md-start mx-auto row-gap-5 column-gap-3 column-gap-md-0">
+          {caracteristicas.map((caracteristica) => (
+            <div
+              className="col-5 text-center d-flex flex-column col-md-4 col-lg-3"
+              key={caracteristica.id}
+            >
+              <div className="w-75 mx-auto">
+                <span className={caracteristica.icon}></span>
+                <p className="m-0 text-break">{caracteristica.nombre}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default TourDetailCaracteristicas;
--- src/Components/TourDetailSection.jsx
+++ src/Components/TourDetailSection.jsx
@@ -1,42 +1,45 @@
-import React from 'react';
-import Rating from './Rating';
-import { dummyData } from '@assets/data/dummyData';
-import Experiencias from './Experiencias';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Rating from "./Rating";
+import { dummyData } from "@assets/data/dummyData";
+import Experiencias from "./Experiencias";
+import { Link } from "react-router-dom";
+import TourDetailCaracteristicas from "./TourDetailCaracteristicas";
 
 const TourDetailSection = ({ id }) => {
   const tour = dummyData.find((e) => e.id_tour === Number(id));
   console.log(tour, id);
   return (
-    <div className='d-flex flex-column gap-3'>
-      <div className='d-flex flex-column justify-content-between gap-2'>
-        <div className='d-flex  justify-content-start m-2'>
-          <Link to={'/'}>
-            <button className='atras-btn'>&lt;- Volver atras</button>
+    <div className="d-flex flex-column gap-3">
+      <div className="d-flex flex-column justify-content-between gap-2">
+        <div className="d-flex  justify-content-start m-2">
+          <Link to={"/"}>
+            <button className="atras-btn">&lt;- Volver atras</button>
           </Link>
         </div>
-        <div className='detail-title-section'>
-          <h2 className='detail-title'> {tour.nombre}</h2>
+        <div className="detail-title-section">
+          <h2 className="detail-title"> {tour.nombre}</h2>
         </div>
       </div>
-      <div className='detail-body gap-3 d-flex flex-column justify-content-center'>
-        <div className=''>
+      <div className="detail-body gap-3 d-flex flex-column justify-content-center">
+        <div className="">
           <img
-            className='detail-img'
+            className="detail-img"
+            style={{ maxWidth: "100%" }}
             src={tour.imagen}
-            alt='tour-photo'
+            alt="tour-photo"
           />
         </div>
-        <div className='d-flex justify-content-center'>
+        <div className="d-flex justify-content-center">
           <Rating />
         </div>
-        <div className='d-flex justify-content-center'>
-          <p className='detail-description'>{tour.descripcion}</p>
+        <div className="d-flex justify-content-center">
+          <p className="detail-description">{tour.descripcion}</p>
         </div>
       </div>
-      <div className='d-flex justify-content-center'>
-        <button className='reservar-btn'>Reservar</button>
+      <div className="d-flex justify-content-center">
+        <button className="reservar-btn">Reservar</button>
       </div>
+      <TourDetailCaracteristicas caracteristicas={tour.caracteristicas} />
       <Experiencias />
     </div>
   );
