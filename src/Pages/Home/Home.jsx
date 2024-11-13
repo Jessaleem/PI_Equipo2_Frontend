e747b1216c8713ab4d59be97d99fdfd68c4c98e5
Quecky <dalisbarranco@hotmail.com>
2024-11-13 13:05:34 -0600

    mobilefixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import ToursDestacados from "../../Components/ToursDestacados";
 
 const Home = () => (
   <section>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <Banner />
     <ToursDestacados />
     <Categorias />
