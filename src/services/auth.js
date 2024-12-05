448a4ef1d2b97c8a6c5f4a3883e6e98dbdbd85ba
ocoyladev <ocoyladev@gmail.com>
2024-12-04 20:02:12 -0500

    Vinculando a servidor backend

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,6 @@
+import { API } from "../provider/api/api";
+
+
 export async function login(auth) {
   const options = {
     method: 'POST',
@@ -8,7 +11,7 @@ export async function login(auth) {
   };
   try {
     const response = await fetch(
-      `http://localhost:8080/api/v1/auth/login`,
+      `${API}/auth/login`,
       options
     );
     const data = await response.json();
@@ -32,7 +35,7 @@ export const register = async (data) => {
     body: JSON.stringify(body),
   };
   try {
-    const response = await fetch(`http://localhost:8080/api/v1/users`, options);
+    const response = await fetch(`${API}/users`, options);
     const data = await response.json();
     return data;
   } catch (error) {
