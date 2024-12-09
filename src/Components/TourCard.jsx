139285941cf014bae7c425217037b4579fb2dc8a
ocoyladev <ocoyladev@gmail.com>
2024-12-08 20:08:49 -0500

    Verificando cambios

--- src/Components/TourCard.jsx
+++ src/Components/TourCard.jsx
@@ -53,6 +53,7 @@ const TourCard = ({ imagen, nombre, id, tourC}) => {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgb(0 0 0 / 50%)" width="30px" height="30px" class="custom-heart" stroke="white" stroke-width="1.5">
                   <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
               </svg>
+              
               </span>  
         } </button>
         <span class="tooltip1">{isFav ? "Quitar de favoritos" : "Agregar a favoritos"}</span>
