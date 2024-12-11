27afe3917bc87a0d02f94241437efe536a382874
Jessica Martinez <aleejess@gmail.com>
2024-12-11 15:46:36 -0500

    feature/reservation-details

--- src/Pages/ReservationDetail/ReservationContent.jsx
+++ src/Pages/ReservationDetail/ReservationContent.jsx
@@ -69,6 +69,7 @@ const ReservationContent =  () => {
         </div>
       </section>
       <button className="reservar-btn" style={{marginTop: 'auto', marginBottom: 'auto'}}>Reservar</button>
+      <p>change</p>
     </div>
     
   </div>
