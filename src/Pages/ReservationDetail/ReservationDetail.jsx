eaf7d44927c6d295dbcd06d16c19b7977af39405
Luis Armuto <luis.backend.fractalup@gmail.com>
2024-12-11 07:41:36 -0500

    responsive, validacion de registro, modales reservas

--- src/Pages/ReservationDetail/ReservationDetail.jsx
+++ src/Pages/ReservationDetail/ReservationDetail.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import { useGeneralContext } from '../../context/useGeneralContext';
+import React, { useState, useEffect } from 'react';
+import Modal from "react-modal";
+import { useMutation } from '@tanstack/react-query';
+
+import { useGeneralContext } from "../../context/useGeneralContext";
 
 const ReservationDetail = () => {
   const { state } = useGeneralContext();
-  console.log('reservationDetails', state.reservationDetails);
-  return <div>ReservationDetail</div>;
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+
+  console.log("reservationDetails", state.reservationDetails);
+
+  const mutation = useMutation({
+    onError: (error) => {
+      if (error.response && error.response.data && error.response.data.message) {
+        setRegistrationError(error.response.data.message);
+      } else {
+        setRegistrationError('Falló en la reserva');
+      }
+      setIsErrorModalOpen(true);
+    },
+  });
+
+  const closeErrorModal = () => {
+    setIsErrorModalOpen(false);
+    setRegistrationError('');
+  };
+
+  
+  return (
+    <div >ReservationDetail</div>
+    //<div className='form-container p-0 min-vh-100'>
+    //  <p>ReservationDetail</p>
+    //  <Modal
+    //    isOpen={isErrorModalOpen}
+    //    onRequestClose={closeErrorModal}
+    //    contentLabel="Error Modal"
+    //    className="error-modal error-message"
+    //    overlayClassName="error-modal-overlay"
+    //  >
+    //    <div
+    //      className="error-container error-container"
+    //    >
+    //      <div className="error-icon">✅</div>
+    //      <h2>¡La reserva ha sido realizada con exito!</h2>
+    //      <button>Cerrar</button>
+    //    </div>
+    //  </Modal>
+//
+    //  <Modal
+    //    isOpen={isErrorModalOpen}
+    //    onRequestClose={closeErrorModal}
+    //    contentLabel="Error Modal"
+    //    className="error-modal error-message"
+    //    overlayClassName="error-modal-overlay"
+    //  >
+    //    <div
+    //      className="error-container error-container"
+    //    >
+    //      <div className="error-icon ">❌</div>
+    //      <h3>Los datos de tu reserva estan incompletos</h3>
+    //      <p>
+    //        Por favor, asegurate de agregar todos los detalles.
+    //      </p>
+    //      <button>Cerrar</button>
+    //    </div>
+    //  </Modal>
+    //</div>
+  );
 };
 
 export default ReservationDetail;
--- src/services/auth.js
+++ src/services/auth.js
@@ -35,9 +35,16 @@ export const register = async (data) => {
   };
   try {
     const response = await fetch(`${API}/users`, options);
-    const data = await response.json();
-    return data;
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error desconocido');
+    }
+    
+    return await response.json();
+
   } catch (error) {
-    console.error(error);
+    console.error('Error en la solicitud:', error);
+    throw error;
   }
 };
